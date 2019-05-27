21c0b816f3523ad07aa62e2b17ac2567acd6f54b
ryanboris <ryan.boris@gmail.com>
2019-05-27 12:29:06 -0400

    adds create link form

--- src/components/Link/CreateLink.js
+++ src/components/Link/CreateLink.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React from 'react'
 
 function CreateLink(props) {
-  return <div>CreateLink</div>;
+  return (
+    <form className="flex flex-column mt3">
+      <input
+        name="description"
+        placeholder="description for your link"
+        autoComplete="off"
+        type="text"
+      />
+      <input
+        name="url"
+        placeholder="URL for your link"
+        autoComplete="off"
+        type="text"
+      />
+      <button className="button" type="submit">
+        Submit
+      </button>
+    </form>
+  )
 }
 
-export default CreateLink;
+export default CreateLink
